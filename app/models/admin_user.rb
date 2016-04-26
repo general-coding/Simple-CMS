0d4008f2d07c788520412f1085966d2fc6c1d0af
Puneeth U Bharadwaj <puneeth.u.bharadwaj@outlook.com>
2016-04-26 03:06:28 -0500

    second commit

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -4,5 +4,7 @@ class AdminUser < ActiveRecord::Base
 	# self.table_name = "admin_users"
 	
 	has_and_belongs_to_many :pages
+	has_many :section_edits
+	has_many :section, :through => :section_edits
 	
 end
--- db/migrate/20160426062700_create_section_edits.rb
+++ db/migrate/20160426062700_create_section_edits.rb
@@ -1,8 +1,15 @@
 class CreateSectionEdits < ActiveRecord::Migration
-  def change
+  def up
     create_table :section_edits do |t|
-
-      t.timestamps null: false
+		t.references :admin_user
+		t.references :section
+		t.string :summary
+		t.timestamps
     end
+	add_index :section_edits, ["admin_user_id", "section_id"]
+  end
+  
+  def down
+	drop_table :section_edits
   end
 end
